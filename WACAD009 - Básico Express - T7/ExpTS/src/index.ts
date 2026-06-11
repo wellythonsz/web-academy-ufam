cc3722785f9fdb121a50e398ed6d1d3f6c31228f
Wellython <wellython.dev@gmail.com>
2026-06-10 23:39:08 -0400

    feat: configura SASS e adiciona cor na view hb1 (Exercício 2 - Parte 10)

--- WACAD009 - Básico Express - T7/ExpTS/src/index.ts
+++ WACAD009 - Básico Express - T7/ExpTS/src/index.ts
@@ -3,36 +3,35 @@ import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv';
 import logger from './middlewares/logger';
 import router from './router';
-import { engine } from 'express-handlebars'; // Importa o Handlebars
-import path from 'path'; // Importa o módulo nativo path do Node
+import { engine } from 'express-handlebars';
+import path from 'path';
+import sass from 'node-sass-middleware'; // 1. Importa o SASS
 
 dotenv.config();
 validateEnv();
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 4444; 
 
-// --- CONFIGURAÇÃO DO HANDLEBARS ---
-app.engine('handlebars', engine({
-  helpers: {
-    // Helper de bloco que recebe o array e filtra por Node.js
-    filtraPorNode: (tecnologias: any[], options: any) => {
-      return tecnologias
-        .filter((tech) => tech.poweredByNodejs) // Filtra apenas os que são true
-        .map((tech) => options.fn(tech))        // Injeta os dados no HTML da página
-        .join('');
-    }
-  }
-}));
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(process.cwd(), 'src', 'views'));
-console.log("🔥 O MOTOR DO HANDLEBARS FOI CONFIGURADO!"); // <-- Adicione esta linha
-// ----------------------------------
 
 app.use(logger('completo'));
 
-// A linha mágica que injeta as rotas
-app.use(router);
+// --- 2. CONFIGURAÇÃO DO SASS E ARQUIVOS ESTÁTICOS ---
+app.use(sass({
+  src: path.join(process.cwd(), 'public', 'scss'),
+  dest: path.join(process.cwd(), 'public', 'css'),
+  outputStyle: 'compressed',
+  prefix: '/css'
+}));
+
+// 3. Libera a pasta 'public' para o navegador acessar o CSS
+app.use('/css', express.static(path.join(process.cwd(), 'public', 'css')));
+// ---------------------------------------------------
+
+app.use(router); // O router continua aqui embaixo
 
 app.listen(PORT, () => {
   console.log(`Express app iniciada na porta ${PORT}.`);
